da1def13042e1ac35123d769897385eb1643a83d
Jauchi <tst@p1ng.link>
2023-09-25 18:35:01 +0200

    removed REPLACE into (will destroy foreign keys)

--- src/parse_tags.py
+++ src/parse_tags.py
@@ -16,39 +16,56 @@ def parse_tagfile(location):
 
 def sync_tags(db: sqlite3.Connection, tags: list):
     c = db.cursor()
-    c.executemany(
-        'INSERT INTO TAGS (id, '
-        'category, description, descriptioneng, parent, hidefromsuggestions, targets, thumbMime) '
-        'VALUES (:id, :categoryName, :description, :descriptionEng, :parent_id, :hideFromSuggestions, :targets, '
-        ':thumbMime) ON CONFLICT DO NOTHING',
-        tags)
+
+    while len(tags) > 0:
+        tag = tags.pop(0)
+        try:
+            c.execute('INSERT INTO TAGS (id, '
+                      'category, description, descriptioneng, parent, hidefromsuggestions, targets, thumbMime) '
+                      'VALUES (:id, :categoryName, :description, :descriptionEng, :parent_id, :hideFromSuggestions, '
+                      ':targets,'
+                      ':thumbMime) ON CONFLICT DO NOTHING', tag)
+        except sqlite3.IntegrityError:
+            # Not all requirements (parent) were added yet, put the tag at the end of the list and try again later...
+            tags.append(tag)
+
     db.commit()
 
 
-# TODO: multi
+# TODO: batch processing
 def link_song_id_to_tag(song_id: int, tag_id: int, c: sqlite3.Cursor):
-    c.execute('INSERT INTO SONGS_TAGS (song_id, tag_id) VALUES (:song_id, :tag_id) ON CONFLICT DO NOTHING', {
-        'song_id': song_id,
-        'tag_id': tag_id
-    })
+    try:
+        c.execute('INSERT INTO SONGS_TAGS (song_id, tag_id) VALUES (:song_id, :tag_id) ON CONFLICT DO NOTHING', {
+            'song_id': song_id,
+            'tag_id': tag_id
+        })
+    except sqlite3.IntegrityError:
+        # Deleted tags do not show up in tags JSON, but still in Songs JSON.
+        pass
 
 
 def sync_tag_names(db: sqlite3.Connection, names: list):
     c = db.cursor()
-    c.executemany('REPLACE INTO TAG_NAMES (tag_id, language, value) VALUES (:tag_id, :language, :value)', names)
+    c.executemany('INSERT INTO TAG_NAMES (tag_id, language, value) VALUES (:tag_id, :language, :value) ON CONFLICT '
+                  'DO NOTHING', names)
     db.commit()
 
 
 def sync_related_tags(db: sqlite3.Connection, related_tags: list):
     c = db.cursor()
-    c.executemany('REPLACE INTO RELATED_TAGS (a,b) VALUES (:a, :b)', related_tags)
+    # TODO: We get inconsistent data from the dump sometimes, so... just ignore it, I guess?
+    for t in related_tags:
+        try:
+            c.execute('INSERT INTO RELATED_TAGS (a,b) VALUES (:a, :b) ON CONFLICT DO NOTHING', t)
+        except sqlite3.IntegrityError:
+            print('Skipping nonexistant tag relationship - A: ' + str(t['a']) + ' B: ' + str(t['b']))
     db.commit()
 
 
 def sync_weblinks(db: sqlite3.Connection, tag_weblinks: list):
     c = db.cursor()
-    c.executemany('REPLACE INTO TAG_WEBLINKS (tag_id, category, description, url, disabled) VALUES '
-                  '(:tag_id, :category, :description, :url, :disabled)', tag_weblinks)
+    c.executemany('INSERT INTO TAG_WEBLINKS (tag_id, category, description, url, disabled) VALUES '
+                  '(:tag_id, :category, :description, :url, :disabled) ON CONFLICT DO NOTHING', tag_weblinks)
     db.commit()
 
 
