a55e718cbb9eb6a3101ee976773ccdaf0caf0af0
Jauchi <tst@p1ng.link>
2023-09-25 18:34:00 +0200

    batch processing for weblinks (performance)

--- src/parse_weblinks.py
+++ src/parse_weblinks.py
@@ -7,6 +7,12 @@ def __add_wl(wl_dict: dict, c: sqlite3.Cursor):
               '(:category, :description, :disabled, :url) ON CONFLICT DO NOTHING', wl_dict)
 
 
+def __add_wls(wl_list: [dict], c: sqlite3.Cursor):
+    print(str('Adding/Updating ' + str(len(wl_list)) + ' weblinks for tags...'))
+    c.executemany("INSERT INTO WEBLINKS (category, description, disabled, url) VALUES "
+                  "(:category, :description, :disabled, :url) ON CONFLICT DO NOTHING", wl_list)
+
+
 # TODO: add batch processing
 def link_song_to_weblinks(song_id, weblink_list, db: sqlite3.Connection):
     c = db.cursor()
@@ -26,18 +32,11 @@ def link_song_to_weblinks(song_id, weblink_list, db: sqlite3.Connection):
                        })
 
 
-def link_tags_to_weblinks(tag_id, weblink_list, cursor: sqlite3.Cursor):
-    for le in weblink_list:
-        if le != {}:
-            __add_wl(le, cursor)
-            cursor.execute('INSERT INTO TAGS_WEBLINKS VALUES (:tag_id, (SELECT link_id FROM WEBLINKS WHERE '
-                           'WEBLINKS.category = :category AND '
-                           'WEBLINKS.description = :description AND '
-                           'WEBLINKS.disabled = :disabled AND '
-                           'WEBLINKS.url = :url)) ON CONFLICT DO NOTHING',
-                           {'tag_id': tag_id,
-                            'category': le.get('category'),
-                            'description': le.get('description'),
-                            'disabled': le.get('disabled'),
-                            'url': le.get('url')
-                            })
+def link_tags_to_weblinks(weblink_list, cursor: sqlite3.Cursor):
+    __add_wls(weblink_list, cursor)
+    cursor.executemany('INSERT INTO TAGS_WEBLINKS VALUES (:tag_id, (SELECT link_id FROM WEBLINKS WHERE '
+                       'WEBLINKS.category = :category AND '
+                       'WEBLINKS.description = :description AND '
+                       'WEBLINKS.disabled = :disabled AND '
+                       'WEBLINKS.url = :url)) ON CONFLICT DO NOTHING',
+                       weblink_list)
