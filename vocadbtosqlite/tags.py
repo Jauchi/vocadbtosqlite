bbbf8415ff5c81e227dd099fe2d935ce8bb46d05
Jauchi <tst@p1ng.link>
2023-10-05 16:42:38 +0200

    added events, event series

--- vocadbtosqlite/tags.py
+++ vocadbtosqlite/tags.py
@@ -111,20 +111,45 @@ def link_albums(entries: list, cursor: sqlite3.Cursor):
             pass
 
 
-# Links your object to a tag. Can cause IntegrityErrors (i.e. will not check if the tag exists) which will need to be
-# handled outside this function.
-def link(tag_id: int, cursor: sqlite3.Cursor, song_id: int = None, album_id: int = None):
-    if song_id:
-        table_name = 'SONGS_TAGS'
-        attribute_name = 'song_id'
-        ext_value = song_id
-    elif album_id:
-        table_name = 'ALBUMS_SONGS'
-        attribute_name = 'album_id'
-        ext_value = album_id
-    else:
-        raise ValueError('You need to provide an ID to link a tag to!')
-    cursor.execute(
-        'INSERT INTO ' + str(table_name) + ' (' + attribute_name + ', tag_id) VALUES (:ext_value, :tag_id) '
-                                                                   'ON CONFLICT DO NOTHING',
-        {'ext_value': ext_value, 'tag_id': tag_id})
+def link_songs(entries: list, cursor: sqlite3.Cursor):
+    for e in entries:
+        try:
+            cursor.execute('''
+                INSERT INTO SONGS_TAGS (song_id, tag_id) VALUES (:song_id, :tag_id) ON CONFLICT DO NOTHING
+                ''', e)
+        except sqlite3.IntegrityError:
+            # Ignore deleted tags
+            pass
+
+
+def link_artists(entries: list, cursor: sqlite3.Cursor):
+    for e in entries:
+        try:
+            cursor.execute('''
+                INSERT INTO ARTISTS_TAGS (artist_id, tag_id) VALUES (:artist_id, :tag_id) ON CONFLICT DO NOTHING
+                ''', e)
+        except sqlite3.IntegrityError:
+            # Ignore deleted tags
+            pass
+
+
+def link_event_series(entries: list, cursor: sqlite3.Cursor):
+    for e in entries:
+        try:
+            cursor.execute('''
+                INSERT INTO EVENT_SERIES_TAGS (series_id, tag_id) VALUES (:series_id, :tag_id) ON CONFLICT DO NOTHING
+                ''', e)
+        except sqlite3.IntegrityError:
+            # Ignore deleted tags
+            pass
+
+
+def link_events(entries: list, cursor: sqlite3.Cursor):
+    for e in entries:
+        try:
+            cursor.execute('''
+                INSERT INTO EVENTS_TAGS (event_id, tag_id) VALUES (:event_id, :tag_id) ON CONFLICT DO NOTHING
+                ''', e)
+        except sqlite3.IntegrityError:
+            # Ignore deleted tags
+            pass
